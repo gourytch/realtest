47a69658506887c728c38df94502e77086d16f91
Kotolegokot <gkotolegokot@gmail.com>
2012-12-18 20:05:30 +0600

    realtest.register_flower works

--- mods/plants/flowers/init.lua
+++ mods/plants/flowers/init.lua
@@ -9,9 +9,9 @@ function realtest.register_flower(name, FlowerDef)
 	local flower = {
 		name = name,
 		description = FlowerDef.description or "Flower",
-		inventory_image = FlowerDef.inventory_image or "wieldhand.png",
+		texture = FlowerDef.texture,
 		grounds = FlowerDef.grounds or {"default:dirt", "default:dirt_with_grass", "default:dirt_with_clay", "default:dirt_with_grass_and_clay"},
-		min_lighting = FlowerDef.min_lighting or 8,
+		grow_light = FlowerDef.grow_light or 8,
 		grow_interval = FlowerDef.grow_interval or 1000,
 		grow_chance = FlowerDef.grow_chance or 5,
 		death_cause = FlowerDef.death_cause or
@@ -27,65 +27,58 @@ function realtest.register_flower(name, FlowerDef)
 				return true
 			end
 			return false
-		end
+		end,
+		extra_definition_items = FlowerDef.extra_definition_items or {}
 	}
-	flower.textures = FlowerDef.textures or {flower.inventory_image}
 	realtest.registered_flowers[name] = flower
-	realtest.registered_flowers_list:insert(name)
-end
-
-for i, flower in ipairs(flowers) do
-	minetest.register_node("flowers:"..flower[2], {
-		description = flower[1],
+	table.insert(realtest.registered_flowers_list,name)
+	local NodeDef = {
+		description = flower.description,
 		drawtype = "plantlike",
-		tiles = {flower[3]},
-		inventory_image = flower[3],
-		wield_image = flower[3],
+		tiles = {flower.texture},
+		inventory_image = flower.texture,
+		wield_image = flower.texture,
 		paramtype = "light",
 		sunlight_propagates = true,
 		walkable = false,
-		groups = {dig_immediate=3,dropping_node=1},
+		groups = {dig_immediate=3,dropping_node=1,flower=1},
 		selection_box = {
 			type = "fixed",
 			fixed = {-0.2, -0.5, -0.2, 0.2, 0.2, 0.2}
 		}
-	})
-	
-	local function grow_flower(pos)
-		if minetest.env:get_node(pos).name ~= "air" then
-			return
-		end
-		if not minetest.env:get_node_light(pos) then
-			return
-		end
-		if minetest.env:get_node_light(pos) < flower[10] then
-			return
-		end
-		if table.contains(flower[11], minetest.env:get_node({x=pos.x,y=pos.y-1,z=pos.z}).name) then
-			minetest.env:add_node(pos, {name = "flowers:"..flower[2]})
-		end
+	}
+	for n, item in pairs(flower.extra_definition_items) do
+		NodeDef[n] = item
 	end
-	
+	minetest.register_node(name, NodeDef)
 	minetest.register_abm({
-		interval = flower[5],
-		chance = flower[6],
-		nodenames = {"flowers:"..flower[2]},
+		interval = flower.grow_interval,
+		chance = flower.grow_chance,
+		nodenames = {name},
 		action = function(pos, node, active_object_count, active_object_count_wider)
-			local n = minetest.env:find_nodes_in_area({x=pos.x-2,y=pos.y-2,z=pos.z-2}, {x=pos.x+2,y=pos.y+2,z=pos.z+2}, node.name)
-			if #n > 5 then
+			local n = minetest.env:find_nodes_in_area({x=pos.x-2,y=pos.y-2,z=pos.z-2}, {x=pos.x+2,y=pos.y+2,z=pos.z+2}, "group:flower")
+			if flower.death_cause(n) then
 				minetest.env:remove_node(pos)
-			elseif #n >= 1 then
-				grow_flower({x=pos.x+math.random(10)-5,y=pos.y+math.random(10)-5,z=pos.z+math.random(10)-5})
+			elseif flower.grow_cause(n) then
+				local p = {x=pos.x+math.random(10)-5,y=pos.y+math.random(10)-5,z=pos.z+math.random(10)-5}
+				if minetest.env:get_node(p).name ~= "air" then
+					return
+				end
+				if not minetest.env:get_node_light(p) then
+					return
+				end
+				if minetest.env:get_node_light(p) < flower.grow_light then
+					return
+				end
+				if table.contains(flower.grounds, minetest.env:get_node({x=p.x,y=p.y-1,z=p.z}).name) then
+					minetest.env:add_node(p, {name = name})
+				end
 			end
 		end,
 	})
-	
-	--[[minetest.register_abm({
-		interval = flower[8],
-		chance = flower[9],
-		nodenames = {"flowers:"..flower[2]},
-		action = function(pos, node, active_object_count, active_object_count_wider)
-			minetest.env:remove_node(pos)
-		end,
-	})]]
 end
+
+realtest.register_flower("flowers:camomile", {
+	description = "Camomile",
+	texture = "flowers_camomile.png",
+})
