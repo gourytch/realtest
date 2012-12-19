e239c3ace4b6f55845332d0d9e204c0ba9c4ed60
Kotolegokot <gkotolegokot@gmail.com>
2012-12-19 19:30:15 +0600

    Added flowers generation

--- mods/plants/flowers/init.lua
+++ mods/plants/flowers/init.lua
@@ -23,6 +23,7 @@ function realtest.register_flower(name, FlowerDef)
 			end
 			return false
 		end,
+		amount_coefficient = FlowerDef.amount_coefficient or 20,
 		extra_definition_items = FlowerDef.extra_definition_items or {}
 	}
 	realtest.registered_flowers[name] = flower
@@ -126,6 +127,7 @@ realtest.register_flower("flowers:grass", {
 		function(flowers_around)
 			return true
 		end,
+	amount_coefficient = 40,
 	extra_definition_items = {
 		selection_box = {
 			type = "fixed",
@@ -197,3 +199,67 @@ minetest.register_node("flowers:pot_with_dirt", {
 	drawtype = "nodebox",
 	paramtype = "light",
 })
+
+local function generate(flower, minp, maxp, seed)
+	flower = realtest.registered_flowers[flower]
+	local perlin1 = minetest.env:get_perlin(seed, 3, 0.6, 100)
+	-- Assume X and Z lengths are equal
+	local divlen = 16
+	local divs = (maxp.x-minp.x)/divlen+1;
+	for divx=0,divs-1 do
+		for divz=0,divs-1 do
+			local x0 = minp.x + math.floor((divx+0)*divlen)
+			local z0 = minp.z + math.floor((divz+0)*divlen)
+			local x1 = minp.x + math.floor((divx+1)*divlen)
+			local z1 = minp.z + math.floor((divz+1)*divlen)
+			-- Determine flowers amount from perlin noise
+			local flowers_amount = math.floor(perlin1:get2d({x=x0, y=z0}) * flower.amount_coefficient)
+			-- Find random positions for flowers based on this random
+			local pr = PseudoRandom(seed)
+			for i=0,flowers_amount do
+				local x = pr:next(x0, x1)
+				local z = pr:next(z0, z1)
+				-- Find ground level (0...30)
+				local ground_y = nil
+				for y=30,0,-1 do
+					if minetest.env:get_node({x=x,y=y,z=z}).name ~= "air" then
+						ground_y = y
+						break
+					end
+				end
+				if ground_y then
+					local p = {x=x,y=ground_y+1,z=z}
+					if minetest.env:get_node(p).name ~= "air" then
+						return
+					end
+					if not minetest.env:get_node_light(p) then
+						return
+					end
+					if minetest.env:get_node_light(p) < flower.grow_light then
+						return
+					end
+					if table.contains(flower.grounds, minetest.env:get_node({x=p.x,y=p.y-1,z=p.z}).name) then
+						minetest.env:add_node(p, {name = flower.name})
+					end
+				end
+			end
+		end
+	end
+end
+
+minetest.register_on_generated(function(minp, maxp, seed)
+	local pr = PseudoRandom(seed)
+	local n = 1
+	if pr:next(1, 5) == 1 then
+		n = n + 1
+	end
+	if pr:next(1, 10) == 1 then
+		n = n + 1
+	end
+	for i = 1, n do
+		generate(realtest.registered_flowers_list[pr:next(1,#realtest.registered_flowers_list)], minp, maxp, seed, 1/8/2, 1) 
+	end
+	if realtest.registered_flowers_list["flowers:grass"] then
+		generate("flowers:grass", minp, maxp, seed, 1/8/2, 1)
+	end
+end)
